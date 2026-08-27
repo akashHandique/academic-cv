903f4452950dbef4da5d1dbf58bb0fd37672c3f3
Akash Jyoti Handique <92847901+akashHandique@users.noreply.github.com>
2026-08-27 03:44:36 +0000

    Fix devcontainer: use existing base image, install Hugo Extended

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+HUGO_VERSION=0.162.0
+curl -sSL -o /tmp/hugo.deb \
+  "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb"
+sudo dpkg -i /tmp/hugo.deb
+
+corepack enable pnpm
+pnpm install --no-frozen-lockfile
+
+hugo version
